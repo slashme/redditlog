1b86c8bc9a6b997b46935b51c22349168e647c10
David Richfield <davidrichfield@gmail.com>
2017-04-03 23:11:38 +0200

    Open and close device; add comments

--- plot.r
+++ plot.r
@@ -1,3 +1,10 @@
+#Read in the log:
 log=read.table("log", colClasses=c("NULL", "NULL", "character", "integer"))
+#I did the timestamps in a bit of a stupid way, so change the text timestamp into a real time:
 log$V3 = as.POSIXlt(log$V3, format = "%H:%M:%S")
+#Set up the plot device:
+png(file = "plot.png", width=1024, height = 1024, units="px", pointsize=30)
+#Plot a snarky graph:
 plot(log, type="l", main="Idiots upvoting Excel graphs", ylab="Score", xlab="Time")
+#Close the device:
+dev.off()
