499e9a32a284f48de8b9ac57259094a6a94d530a
David Richfield <davidrichfield@gmail.com>
2017-04-03 22:55:15 +0200

    Graph labels and line

--- plot.r
+++ plot.r
@@ -1,3 +1,3 @@
 log=read.table("log", colClasses=c("NULL", "NULL", "character", "integer"))
 log$V3 = as.POSIXlt(log$V3, format = "%H:%M:%S")
-plot(log)
+plot(log, type="l", main="Idiots upvoting Excel graphs", ylab="Score", xlab="Time")
