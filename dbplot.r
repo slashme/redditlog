ff6885092bf1bd2457f7c96fda7e263be18d70c8
David Richfield <davidrichfield@gmail.com>
2017-04-04 21:33:01 +0200

    Tweak plot

--- dbplot.r
+++ dbplot.r
@@ -1,6 +1,7 @@
 library("RSQLite")
 con = dbConnect(drv="SQLite", dbname="redditdata.db")
-res = dbGetQuery(con, 'SELECT a.timestamp, a.score, c.created, c.id FROM scores a LEFT JOIN posts c ON a.postid=c.id')
-res$age = res$timestamp - res$created
+res = dbGetQuery(con, 'SELECT a.timestamp, a.score, a.num_comments, c.created, c.id FROM scores a LEFT JOIN posts c ON a.postid=c.id')
+res$age = (res$timestamp - res$created)/60/60/24
+plot(res$score ~ res$age, main="Trajectories", ylab="Score (net upvotes)", xlab="Age [days]")
 #dbClearResult(dbListResults(con)[[1]])
 
