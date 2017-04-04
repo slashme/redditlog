3861000c0d0d347b20bd286998ae1b5d823b6d6d
David Richfield <davidrichfield@gmail.com>
2017-04-04 19:27:14 +0200

    Use database to store post and score info

--- monitor.py
+++ monitor.py
@@ -1,15 +1,29 @@
-import requests, json
+import requests, json, time, sqlite3
 
-#The attributes of the post to collect:
-infolist=('subreddit', 'id', 'score', 'title', 'num_comments', 'created_utc', 'ups', 'downs')
+#Database connection:
+conn = sqlite3.connect('redditdata.db')
+c = conn.cursor()
 
 #Request parameters:
-payload = {'limit': '100'}
+payload = {'limit': '1'}
 
-#Grab the data:
-r = requests.get(r'http://www.reddit.com/r/dataisbeautiful/.json', params=payload)
+#Main loop:
+while(True):
+    #Grab the data:
+    r = requests.get(r'http://www.reddit.com/r/dataisbeautiful/.json', params=payload)
+    #Skip parsing if there's an error response:
+    if not ('error' in r.json()): 
+        #Iterate over all the returned items:
+        for i in r.json()['data']['children']:
+            #Skip non-posts:
+            if(i['kind']=='t3'):
+                #Insert the post into the DB, unless it already exists:
+                c.execute("INSERT OR IGNORE INTO posts(id, title, created) VALUES(?,?,?)", (i['data']['id'], i['data']['title'], i['data']['created_utc']))
+                #Log data into the scores table:
+                c.execute("INSERT INTO scores(score, num_comments, ups, downs, postid) VALUES(?,?,?,?,?)", (i['data']['score'], i['data']['num_comments'], i['data']['ups'], i['data']['downs'], i['data']['id'], i['data']['gilded']))
+                conn.commit()
+    #Delay before reloading:
+    time.sleep(20)
 
-for i in r.json()['data']['children']:
-    if(i['kind']=='t3'):
-        for j in infolist:
-            print(j, i['data'][j])
+conn.close()
+
