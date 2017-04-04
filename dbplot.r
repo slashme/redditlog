fa1187b5597c13e9b88d7debf1963cabe38eaa32
David Richfield <davidrichfield@gmail.com>
2017-04-04 23:46:48 +0200

    Legend

--- dbplot.r
+++ dbplot.r
@@ -5,6 +5,10 @@ res$age = (res$timestamp - res$created)/60/60/24
 plot(res$score ~ res$age, main="Trajectories", ylab="Score (net upvotes)", xlab="Age [days]")
 #dbClearResult(dbListResults(con)[[1]])
 #Create empty plot:
-plot(1, type="n", main="Trajectories", ylab="Score (net upvotes)", xlab="Age [days]", xlim=range(res$age), ylim=range(res$score))
+plot(1, type="n",
+     main="Trajectories", ylab="Score (net upvotes)", xlab="Age [days]",
+     xlim=range(res$age), ylim=range(res$score)
+     )
 for (i in unique(res[res$subreddit == "AskReddit", ]$id)) { lines(res[res$id == i, ]$age, res[res$id == i, ]$score, col="green")}
 for (i in unique(res[res$subreddit == "dataisbeautiful", ]$id)) { lines(res[res$id == i, ]$age, res[res$id == i, ]$score, col="red")}
+legend('topleft', c("AskReddit", "dataisbeautiful"), lty=1, col=c("green", "red"))
