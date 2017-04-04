5b08a7d655e45e327649a51476cc187b4cf05aab
David Richfield <davidrichfield@gmail.com>
2017-04-04 22:30:29 +0200

    Plot trajectories per post id

--- dbplot.r
+++ dbplot.r
@@ -1,7 +1,10 @@
 library("RSQLite")
 con = dbConnect(drv="SQLite", dbname="redditdata.db")
-res = dbGetQuery(con, 'SELECT a.timestamp, a.score, a.num_comments, c.created, c.id FROM scores a LEFT JOIN posts c ON a.postid=c.id')
+res = dbGetQuery(con, 'SELECT a.timestamp, a.score, a.num_comments, c.created, c.id, c.subreddit FROM scores a LEFT JOIN posts c ON a.postid=c.id')
 res$age = (res$timestamp - res$created)/60/60/24
 plot(res$score ~ res$age, main="Trajectories", ylab="Score (net upvotes)", xlab="Age [days]")
 #dbClearResult(dbListResults(con)[[1]])
-
+#Create empty plot:
+plot(1, type="n", main="Trajectories", ylab="Score (net upvotes)", xlab="Age [days]", xlim=range(res$age), ylim=range(res$score))
+for (i in unique(res[res$subreddit == "AskReddit", ]$id)) { lines(res[res$id == i, ]$age, res[res$id == i, ]$score, col="green")}
+for (i in unique(res[res$subreddit == "dataisbeautiful", ]$id)) { lines(res[res$id == i, ]$age, res[res$id == i, ]$score, col="red")}
