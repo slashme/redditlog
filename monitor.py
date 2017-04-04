8c32372afb54a8df990b7ee3701d4831a14c77d2
David Richfield <davidrichfield@gmail.com>
2017-04-04 21:45:54 +0200

    Grab two different subreddits; don't ignore self-posts

--- monitor.py
+++ monitor.py
@@ -9,20 +9,24 @@ payload = {'limit': '100'}
 
 #Main loop:
 while(True):
-    #Grab the data:
-    r = requests.get(r'http://www.reddit.com/r/dataisbeautiful/.json', params=payload)
-    requesttime=time.time()
-    #Skip parsing if there's an error response:
-    if not ('error' in r.json()): 
-        #Iterate over all the returned items:
-        for i in r.json()['data']['children']:
-            #Skip non-posts and text posts and stickied posts:
-            if(i['kind']=='t3' and i['data']['is_self']==False and i['data']['stickied']==False):
-                #Insert the post into the DB, unless it already exists:
-                c.execute("INSERT OR IGNORE INTO posts(subreddit, id, title, created) VALUES(?,?,?,?)", (i['data']['subreddit'], i['data']['id'], i['data']['title'], i['data']['created_utc']))
-                #Log data into the scores table:
-                c.execute("INSERT INTO scores(timestamp, score, num_comments, ups, downs, postid, gilded) VALUES(?,?,?,?,?,?,?)", (requesttime, i['data']['score'], i['data']['num_comments'], i['data']['ups'], i['data']['downs'], i['data']['id'], i['data']['gilded']))
-            conn.commit()
+    #Grab info from two different subreddits:
+    for subred in ("dataisbeautiful", "askreddit"):
+        #Grab the data:
+        r = requests.get(r'http://www.reddit.com/r/'+subred+'/.json', params=payload)
+        print(r)
+        print(subred)
+        requesttime=time.time()
+        #Skip parsing if there's an error response:
+        if not ('error' in r.json()): 
+            #Iterate over all the returned items:
+            for i in r.json()['data']['children']:
+                #Skip non-posts and text posts and stickied posts:
+                if(i['kind']=='t3' and i['data']['stickied']==False):
+                    #Insert the post into the DB, unless it already exists:
+                    c.execute("INSERT OR IGNORE INTO posts(subreddit, id, title, created) VALUES(?,?,?,?)", (i['data']['subreddit'], i['data']['id'], i['data']['title'], i['data']['created_utc']))
+                    #Log data into the scores table:
+                    c.execute("INSERT INTO scores(timestamp, score, num_comments, ups, downs, postid, gilded) VALUES(?,?,?,?,?,?,?)", (requesttime, i['data']['score'], i['data']['num_comments'], i['data']['ups'], i['data']['downs'], i['data']['id'], i['data']['gilded']))
+                conn.commit()
     #Delay before reloading:
     time.sleep(120)
 
