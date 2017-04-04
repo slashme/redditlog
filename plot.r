41de59a9414a209d4f682151758dfca1aa657377
David Richfield <davidrichfield@gmail.com>
2017-04-04 07:30:10 +0200

    Timezone and title

--- plot.r
+++ plot.r
@@ -1,7 +1,7 @@
 #Read in the log:
 log=read.table("log", colClasses=c(rep("character", 3), "integer"))
 #I did the timestamps in a bit of a stupid way, so change the text timestamp into a real time:
-log$V3 = as.POSIXlt(paste(log$V1, log$V2, log$V3), format = "%b %d %H:%M:%S", tz="CET")
+log$V3 = as.POSIXlt(paste(log$V1, log$V2, log$V3), format = "%b %d %H:%M:%S", tz="UTC")
 #Set up the plot device:
 png(file = "plot.png", width=1024, height = 1024, units="px", pointsize=30)
 #Plot a snarky graph:
