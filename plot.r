4bc873f7a807db940bcfeca2f844829b4294fcde
David Richfield <davidrichfield@gmail.com>
2017-04-04 07:01:44 +0200

    Fix graph axes and title
    Start y axis at 0
    Use total date stamp for time
    Less insulting title.

--- plot.r
+++ plot.r
@@ -1,10 +1,10 @@
 #Read in the log:
-log=read.table("log", colClasses=c("NULL", "NULL", "character", "integer"))
+log=read.table("log", colClasses=c(rep("character", 3), "integer"))
 #I did the timestamps in a bit of a stupid way, so change the text timestamp into a real time:
-log$V3 = as.POSIXlt(log$V3, format = "%H:%M:%S")
+log$V3 = as.POSIXlt(paste(log$V1, log$V2, log$V3), format = "%b %d %H:%M:%S")
 #Set up the plot device:
 png(file = "plot.png", width=1024, height = 1024, units="px", pointsize=30)
 #Plot a snarky graph:
-plot(log, type="l", main="Idiots upvoting Excel graphs", ylab="Score", xlab="Time", ylim=c(0,max(log$V4)))
+plot(log$V3, log$V4, type="l", main="Default Excel graphs are beautiful data", ylab="Score (net upvotes)", xlab="Time", ylim=c(0,max(log$V4)))
 #Close the device:
 dev.off()
