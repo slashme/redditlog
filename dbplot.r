331ea0e400709d795fa5f3ec443d0b25e9d1c390
David Richfield <davidrichfield@gmail.com>
2017-04-05 21:37:44 +0200

    Misc. plotting tweaks
    Set up PNG plot device; set up cutoff for post age; etc.

--- dbplot.r
+++ dbplot.r
@@ -7,11 +7,20 @@ res = dbGetQuery(con, 'SELECT a.timestamp, a.score, a.num_comments, c.created, c
 #Create a column with the age in days of the post at the time of sampling:
 res$age = (res$timestamp - res$created)/60/60/24
 
+#Age value where we cut off the X-axis:
+cutoff=3.0
+
+#Set up graphics device:
+png(file = "plot.png", width=1024, height = 1024, units="px", pointsize=30)
+
 #Create empty plot:
 plot(1, type="n",
-     main="Trajectories", ylab="Score (net upvotes)", xlab="Age [days]",
-     xlim=c(0,6), ylim=range(res$score)
+     main="Reddit post karma trajectories", ylab="Score (net upvotes)", xlab="Age [days]",
+     #xlim=c(0,cutoff), ylim=range(res$score),
+     xlim=c(0,cutoff), ylim=c(0,30000),
+     xaxs="i", yaxs="i"
      )
+
 #Which subreddits to plot in which colours:
 plotlist = list(
 The_Donald      ="red",
@@ -22,8 +31,11 @@ dataisbeautiful ="green"
 #Iterate over the listed subreddits, plotting the data for each:
 for (r in names(plotlist)) {
   for (i in unique(res[res$subreddit == r, ]$id)) {
-    lines(res[res$id == i, ]$age, res[res$id == i, ]$score, col=as.character(plotlist[r]))
+    if (min(res[res$id == i, ]$age) < cutoff) {
+      lines(res[res$id == i, ]$age, res[res$id == i, ]$score, col=as.character(plotlist[r]))
+    } else {
+    }
   }
 }
 #Create a legend with the names and colors:
-legend('topleft', names(plotlist), lty=1, col=as.character(plotlist))
+legend('topright', names(plotlist), lty=1, col=as.character(plotlist))
