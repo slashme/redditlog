3ffe869bd84ba6a42c4a4cb279271a383b3e4114
David Richfield <davidrichfield@gmail.com>
2017-04-05 18:35:24 +0200

    Change plotting sequence

--- dbplot.r
+++ dbplot.r
@@ -7,9 +7,9 @@ plot(res$score ~ res$age, main="Trajectories", ylab="Score (net upvotes)", xlab=
 #Create empty plot:
 plot(1, type="n",
      main="Trajectories", ylab="Score (net upvotes)", xlab="Age [days]",
-     xlim=range(res$age), ylim=range(res$score)
+     xlim=c(0,6), ylim=range(res$score)
      )
+for (i in unique(res[res$subreddit == "The_Donald", ]$id)) { lines(res[res$id == i, ]$age, res[res$id == i, ]$score, col="red")}
 for (i in unique(res[res$subreddit == "AskReddit", ]$id)) { lines(res[res$id == i, ]$age, res[res$id == i, ]$score, col="blue")}
 for (i in unique(res[res$subreddit == "dataisbeautiful", ]$id)) { lines(res[res$id == i, ]$age, res[res$id == i, ]$score, col="green")}
-for (i in unique(res[res$subreddit == "The_Donald", ]$id)) { lines(res[res$id == i, ]$age, res[res$id == i, ]$score, col="red")}
 legend('topleft', c("AskReddit", "dataisbeautiful", "The_Donald"), lty=1, col=c("blue", "green", "red"))
