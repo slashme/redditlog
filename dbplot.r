4d72a9c98f62e87f8a7e07f6a9e4a0b41a1fb1bc
David Richfield <davidrichfield@gmail.com>
2017-04-07 23:23:32 +0200

    Simplify selection of parameters to plot
    Make it easy to select which combination of age, score, rank and number
    of comments should be plotted.

--- dbplot.r
+++ dbplot.r
@@ -25,11 +25,11 @@ plot(1, type="n",
 
 #Which subreddits to plot in which colours:
 plotlist = list(
-The_Donald      ="red",
+The_Donald      ="pink",
 AskReddit       ="blue",
-IAmA            ="black",
-gonewild        ="pink",
-dataisbeautiful ="green"
+#IAmA            ="black",
+gonewild        ="red"
+#dataisbeautiful ="green",
 )
 
 #Iterate over the listed subreddits, plotting the data for each:
@@ -38,7 +38,6 @@ for (r in names(plotlist)) {
     resi=res[res$id == i, ]
     if (min(resi$age) < cutoff) {
       lines(resi$age, resi$score, col=as.character(plotlist[r]))
-    } else {
     }
   }
 }
@@ -50,30 +49,37 @@ dev.off()
 #Set up graphics device:
 #png(file = "3dplot.png", width=1024, height = 1024, units="px", pointsize=20)
 
+#Set up matrix to hold the coordinates of the corners of the plot:
+corners = matrix(rep(0,8), nrow = 2, ncol = 4, byrow = TRUE,
+  dimnames = list(c("min", "max"),
+                  c("Age [days]", "Score (net karma)", "Position in top 100 posts", "Comments")))
+#And a list to hold the actual parameters (there must be a better way…)
+parnames = c("age", "score", "rank", "num_comments")
 #Corners of the plot:
-mina=min(res$age)
-maxa=max(res$age)
-mins=min(res$score)
-maxs=max(res$score)
-minr=min(res$rank)
-maxr=max(res$rank)
-corners = matrix(c(mina,mins,minr, maxa,maxs,maxr), nrow = 2, ncol = 3, byrow = TRUE,
-               dimnames = list(c("row1", "row2"),
-                               c("Age [days]", "Score (net karma)", "Position in top 100 posts")))
+for (r in names(plotlist)) {
+    resi=res[res$subreddit == r, ]
+    corners[1,1]=min(mina, resi$age)
+    corners[2,1]=max(maxa, resi$age)
+    corners[1,2]=min(mins, resi$score)
+    corners[2,2]=max(maxs, resi$score)
+    corners[1,3]=min(minr, resi$rank)
+    corners[2,3]=max(maxr, resi$rank)
+    corners[1,4]=min(minc, resi$num_comments)
+    corners[2,4]=max(maxc, resi$num_comments)
+}
 
-#Set up graphics device:
-#png(file = paste0("3dplot", ang, ".png"), width=1024, height = 1024, units="px", pointsize=20)
+#Grab only 3 axes (edit the next two lines to choose axes - note the comma!):
+corners = corners[,-3]
+parnames = parnames[-3]
 
-#Create an empty 3D plot:
-#s3d = scatterplot3d(corners, color="white", angle=ang)
+#Create an empty 3D plot: FIXME: Can I plot invisible dots? Is there a better way?
 plot3d(corners, col="white")
 
 #Iterate over the listed subreddits, plotting the data for each:
 for (r in names(plotlist)) {
   for (i in unique(res[res$subreddit == r, ]$id)) {
     resi=res[res$id == i, ]
-    #s3d$points3d(resi$age, resi$score, resi$rank, type="l", col=as.character(plotlist[r]))
-    lines3d(resi$age, resi$score, resi$rank, col=as.character(plotlist[r]))
+    lines3d(resi[,parnames[1]], resi[,parnames[2]], resi[,parnames[3]], col=as.character(plotlist[r]))
   }
 }
 #dev.off()
